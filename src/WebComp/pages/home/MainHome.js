e94a2d40a229e7912f6448e871a37bd8311b94a8
Ryan Ariana <out1688@fifgroup.co.id>
2024-03-21 19:42:24 +0700

    proxy

--- src/WebComp/pages/home/MainHome.js
+++ src/WebComp/pages/home/MainHome.js
@@ -14,7 +14,7 @@ const MainHome = () => {
         getNews();
     }, []);
     const getNews = async () => {
-        axios.get('https://cxtrv.my.id/users', {
+        axios.get('https://cxtrv.my.id/news', {
             withCredentials: true
         })
             .then(function (response) {
